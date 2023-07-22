d1b4ac628304c03fe4fe4978ad721119680fe38a
Priyasaraswat <91541835+Priyasaraswat@users.noreply.github.com>
2023-07-22 11:13:25 +0530

    mongo changes

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose");
 mongoose.set("strictQuery", false);
 const connectDatabase =async()=>
 {
-    await mongoose.connect(process.env.DB_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
+    await mongoose.connect(process.env.DB_URL,{useNewUrlParser:true,useUnifiedTopology:true,family: 4,}).then((data)=>{
         console.log(`Database connected !! `)
     })
    .catch((err)=>{

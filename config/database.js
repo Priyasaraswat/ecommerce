c724720e96942a70f35b9cd7c23546ee70c0c415
Priyasaraswat <91541835+Priyasaraswat@users.noreply.github.com>
2023-07-22 10:38:21 +0530

    chh

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 const mongoose=require("mongoose");
 
 mongoose.set("strictQuery", false);
-const connectDatabase =()=>
+const connectDatabase =async()=>
 {
-    mongoose.connect(process.env.DB_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
-        console.log(`Database connected !! ${data.connection.host}`)
+    await mongoose.connect(process.env.DB_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
+        console.log(`Database connected !! `)
     })
-    //  }).catch((err)=>{
-    //      console.log(err)
-    //  })
+   .catch((err)=>{
+        console.log(err)
+   })
 
 }
 module.exports = connectDatabase
